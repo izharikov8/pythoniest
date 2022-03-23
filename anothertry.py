668fa3490487f214d872e266f0f1a77b419d12ef
Ivan Zharikov <izharikov888@gmail.com>
2022-03-23 21:07:32 +0300

    Just checking

--- anothertry.py
+++ anothertry.py
@@ -1,8 +1,3 @@
-Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> # PY-46 Zharikov Ivan
-# Task 3
-
 queries = [
     'смотреть сериалы онлайн',
     'новости спорта',
@@ -31,5 +26,7 @@ for key, value in data.items():
   print(f'Поисковых запросов состоящих из {key} слова: {percentage}%')
 print()
 
-print("Напечатай комит")
+print('Anything')
+
+
 
